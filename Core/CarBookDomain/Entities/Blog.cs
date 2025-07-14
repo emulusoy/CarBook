ba493610a9744e81af24cb6f63be005108f98c29
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-07-15 01:28:10 +0300

    '

--- Core/CarBookDomain/Entities/Blog.cs
+++ Core/CarBookDomain/Entities/Blog.cs
@@ -10,7 +10,6 @@ namespace CarBook.Domain.Entities
     {
         public int BlogID { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
 
         public int AuthorID { get; set; }
         public Author Author { get; set; }
@@ -21,7 +20,9 @@ namespace CarBook.Domain.Entities
 
         public int CategoryID { get; set; }
         public Category Category { get; set; }
+        public string Description { get; set; }
         public List<TagCloud> TagClouds { get; set; }
+
 
     }
 }
