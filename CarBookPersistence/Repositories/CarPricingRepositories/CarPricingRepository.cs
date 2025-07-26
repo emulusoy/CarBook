983ec82b2a628daa45c24abd62f4263f6a8b39d9
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-07-26 18:06:09 +0300

    nice proggress

--- CarBookPersistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
+++ CarBookPersistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CarBook.Application.Interfaces.CarPricingInterfaces;
+using CarBook.Application.ViewModels;
 using CarBook.Domain.Entities;
 using CarBookPersistence.Context;
 using Microsoft.EntityFrameworkCore;
@@ -25,5 +26,42 @@ namespace CarBookPersistence.Repositories.CarPricingRepositories
             return values;
         }
 
+        public List<CarPricing> GetCarPricingWithTimePeriod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CarPricingViewModel> GetCarPricingWithTimePeriod1()
+        {
+            List<CarPricingViewModel> values = new List<CarPricingViewModel>();
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "Select * From (Select Model,Name,CoverImageUrl,PricingID,Amount From CarPricings Inner Join Cars On Cars.CarID=CarPricings.CarId Inner Join Brands On Brands.BrandID=Cars.BrandID) As SourceTable Pivot (Sum(Amount) For PricingID In ([2],[3],[4])) as PivotTable;";
+                command.CommandType = System.Data.CommandType.Text;
+                _context.Database.OpenConnection();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        CarPricingViewModel carPricingViewModel = new CarPricingViewModel()
+                        {
+                            Brand = reader["Name"].ToString(),
+                            Model = reader["Model"].ToString(),
+                            CoverImageUrl = reader["CoverImageUrl"].ToString(),
+                            Amounts = new List<decimal>
+                            {
+                                Convert.ToDecimal(reader["2"]),
+                                Convert.ToDecimal(reader["3"]),
+                                Convert.ToDecimal(reader["4"])
+                            }
+                        };
+                        values.Add(carPricingViewModel);
+                    }
+                }
+                _context.Database.CloseConnection();
+                return values;
+            }
+
+        }
     }
 }
---
+++ Frontends/CarBook.WebUI/Controllers/CarPricingController.cs
@@ -0,0 +1,36 @@
+﻿using CarBook.Dto.BlogDtos;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using CarBook.Dto.CarPricingDtos;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class CarPricingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarPricingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.PageTitle = "Car Pricing";
+            ViewBag.PageDescription = "Car Pricing Information";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7028/api/CarPricings/GetCarPricingWithTimePeriodList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
